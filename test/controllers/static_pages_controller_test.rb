45014e4a170e74350bb460450f4e0d6cda9fbc29
Arthlight <arthurpbh@gmail.com>
2020-11-07 16:46:46 +0100

    Added Integration tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,25 +9,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_path
     assert_response :success
-    assert_select "title", {count: 1, text: "#{@base_title}"}
+    assert_select "title", text: "#{@base_title}"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", {count: 1, text: "Help | #{@base_title}"}
+    assert_select "title", text: "Help | #{@base_title}"
+    # assert_select "title", count: 1, text: "Help | #{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", {count: 1, text: "About | #{@base_title}"}
+    assert_select "title", count: 1, text: "About | #{@base_title}"
   end
 
   test"should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", {count: 1, text: "Contact | #{@base_title}"}
+    assert_select "title", count: 1, text: "Contact | #{@base_title}"
   end
 
 end
